a444fa1332c22fd24e21990f4c080485c0f9662f
ypavlou <50519804+ypavlou@users.noreply.github.com>
2019-12-01 16:40:41 +0200

    Add files via upload

---
+++ InputImplementatition.cpp
@@ -0,0 +1,191 @@
+#include "InputImplementatition.h"
+
+template <class inputData>
+void InputGenericVector<inputData>::push (inputData const& value) {
+
+    temp.push_back(value);
+}
+template <class inputData>
+void InputGenericVector<inputData>::save () {
+    if(temp!=previous)
+        itemValues.push_back(make_pair(itemID,temp));
+    previous.clear();
+    previous.shrink_to_fit();
+    previous.resize(temp.size());
+    previous = temp;
+    temp.clear();
+}
+template <>
+void InputGenericVector<double>::printVector(){
+    for (unsigned int i=0; i<itemValues.size(); i++){
+        //cout<<itemValues[i].second.size()<<endl;
+        cout<<itemValues[i].first<<endl;
+        for(unsigned int j=0; j<itemValues[i].second.size(); j++){
+            cout<<itemValues[i].second.at(j)<<" ";
+        }
+        cout<<endl;
+    }
+}
+template <>
+void InputGenericVector<pair<double,double>>::printVector(){
+    for (unsigned int i=0; i<itemValues.size(); i++){
+        //cout<<itemValues[i].second.size()<<endl;
+        cout<<itemValues[i].first<<endl;
+        cout.precision(numeric_limits< double >::max_digits10);
+        for(unsigned int j=0; j<itemValues[i].second.size(); j++){
+            cout<<"("<<itemValues[i].second.at(j).first<<","<<itemValues[i].second.at(j).second<<") ";
+        }
+        cout<<endl;
+    }
+}
+template <>
+void InputGenericVector<pair<double,double>>::maxCoordFinder(double& max){
+    for (unsigned int i=0; i<itemValues.size(); i++){
+        for(unsigned int j=0; j<itemValues[i].second.size(); j++){
+            if(max<itemValues[i].second.at(j).first)
+                max=itemValues[i].second.at(j).first;
+            if(max<itemValues[i].second.at(j).second)
+                max=itemValues[i].second.at(j).second;
+        }
+    }
+}
+template <>
+void InputGenericVector<double>::constructorFunction(InputGenericVector<double>& vector,string const& value){
+    vector.push(stod(value));
+
+}
+template <>
+void InputGenericVector<pair<double,double>>::constructorFunction(InputGenericVector<pair<double,double>>& vector,string const& value){
+    stringstream stream(value);
+    pair<double ,double > vector_value;
+    string temp;
+    getline(stream, temp, ' ');
+    vector_value.first=stod(temp);
+    getline(stream, temp, ' ');
+    vector_value.second=stod(temp);
+    vector.push(vector_value);
+}
+
+template <class inputData>
+InputGenericVector<inputData>::InputGenericVector(string const& path){ //For input file
+    //The input file
+
+    ifstream inputFile;
+    try{
+        inputFile.open(path);
+        if (!inputFile.is_open())
+            throw "Can't open file";
+        string firstLine;
+        getline(inputFile, firstLine);  //read first line
+        while (!inputFile.eof()) {
+            string sLine;
+            //Read line by line
+            getline(inputFile, sLine);
+            if(sLine.length()==0)//Break if it's last line.
+                break;
+
+
+            bool itemIDflag= true;
+            string word = "";
+            for (auto& c:sLine ) {
+                //Push every value delimited by white space
+                if (iswspace(c)) {
+                    if(!itemIDflag) { //If it is not the item's id
+                        //this->push(stoi(sLine.substr(0, pos)));
+                        constructorFunction(*this, word);           //push the created string
+                    }
+                    else{
+                        itemID=word;
+                        itemIDflag= false;
+                    }
+                    word = "";
+                }
+                else {
+                    word = word + c;
+                }
+
+            }
+            //for the last word of line
+            if(!itemIDflag) { //If it is not the item's id
+                //this->push(stoi(sLine.substr(0, pos)));
+                constructorFunction(*this, word);
+            }
+            else{
+                itemID=word;
+                itemIDflag= false;
+            }
+            //Save the pushed values
+            this->save();
+        }
+
+        inputFile.close();
+    }catch (const char* msg) {
+        cerr << msg << endl;
+    }
+}
+
+template <>
+InputGenericVector<pair<double,double>>::InputGenericVector(string const& path,unsigned int& maxCurveSize,unsigned int& minCurveSize,bool const& input){
+    //The input file
+    previous.emplace_back(make_pair(0,0));
+    ifstream inputFile;
+    try {
+        inputFile.open(path);
+        if (!inputFile.is_open())
+            cout << "Can't open file" << endl;
+
+        unsigned int max_m = 0; //Max size of all grids
+        unsigned int min_m = 4294967295;//Min size of all grids
+        string firstLine;
+        getline(inputFile, firstLine);  //read first line
+        bool prevFlag = true;
+
+        while (!inputFile.eof()) {
+            string sLine;
+            //Read line by line
+            getline(inputFile, sLine);
+
+            if (sLine.length() == 0)//Break if it's last line=empty line.
+                break;
+            size_t pos = 0;
+
+            while ((pos = sLine.find('\t')) != string::npos) {
+                itemID = sLine.substr(0, pos);
+
+                sLine.erase(0, pos + 1);
+                pos = sLine.find('\t');
+                unsigned int curve_size = (unsigned int) stoi(sLine.substr(0, pos));
+                if (curve_size > max_m)
+                    max_m = curve_size;
+                if (curve_size < min_m)
+                    min_m = curve_size;
+                sLine.erase(0, pos + 1);
+                for (unsigned int i = 0; i < curve_size; i++) {
+                    pos = sLine.find(',');
+                    string temp_str = sLine.substr(0, pos);
+                    temp_str.erase(0, 1); //Remove first character of string which is (
+
+                    sLine.erase(0, pos + 1);
+                    pos = sLine.find(')');
+                    temp_str += sLine.substr(0, pos);
+                    //cout<<"to temp pou stelnw "<<temp<<endl;
+                    constructorFunction(*this, temp_str);
+                    sLine.erase(0, pos + 2);
+                }
+            }
+            //Save the pushed values
+            this->save();
+        }
+
+        inputFile.close();
+        if (max_m > maxCurveSize)
+            maxCurveSize = max_m;
+        if (min_m < minCurveSize)
+            minCurveSize = min_m;
+    } catch (const char *msg) {
+        cerr << msg << endl;
+    }
+
+}
+template class InputGenericVector<double>;
+template class InputGenericVector<pair<double,double>>;
